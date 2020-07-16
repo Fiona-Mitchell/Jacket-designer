6a89ad9d1f39d26b3268a1a6b9706e9e58422804
Fiona-Mitchell <fionamitchelldesign@gmail.com>
2020-07-16 21:18:43 +0100

    Math.random
    Using the Math.random to return random integers which will generate random RGB values, thereby randomly changing the colour

--- js.js
+++ js.js
@@ -4,3 +4,12 @@ for(let i = 0; i < children.length; i++ ){
   children[i].setAttribute('fill',getRandomColor());
 }
 }
+
+function getRandomColor() {
+var letters = '0123456789ABCDEF';
+var color = '#';
+for (var i = 0; i < 6; i++) {
+  color += letters[Math.floor(Math.random() * 16)];
+}
+return color;
+}
